de320bc5a1bf1eff3338f462db352faa40147727
SongMinGyu0506 <ggp04114@naver.com>
2021-12-26 00:23:44 +0900

    restServer.js 기록

--- 4/restServer.js
+++ 4/restServer.js
@@ -1,16 +1,18 @@
 const http = require('http');
 const fs = require('fs').promises;
 
-const users = {};
+const users = {}; // DB 대체
 
 http.createServer(async (req,res)=>{
     try{
-        console.log(req.method,req.url);
+        console.log(req.method,req.url); // ex) GET /
+
+        //GET ==> 해당 코드에서는 페이지 이동
         if(req.method === 'GET') {
             if(req.url === '/') {
                 const data = await fs.readFile('./4/restFront.html');
                 res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'});
-                return res.end(data);
+                return res.end(data); //data 출력 후 종료
             } else if (req.url ==='/about') {
                 const data = await fs.readFile('./4/about.html');
                 res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'});
@@ -23,15 +25,20 @@ http.createServer(async (req,res)=>{
                 const data = await fs.readFile(`.${req.url}`);
                 return res.end(data);
             } catch (err) {
-
+                console.error(err)
             }
+        //POST 데이터 입력시 사용
         } else if (req.method === 'POST') {
             if (req.url === '/user') {
                 let body = '';
+
+                //본문에 들어있는 데이터를 꺼내는 작업
                 req.on('data',(data)=> {
                     body += data;
                     console.log(body);
                 });
+
+                //종료할 때 users에 저장
                 return req.on('end', ()=>{
                     console.log('POST 본문(Body):',body);
                     const { name } = JSON.parse(body);
@@ -44,21 +51,21 @@ http.createServer(async (req,res)=>{
         } 
         else if (req.method === 'PUT') {
             if(req.url.startsWith('/user/')) {
-                const key = req.url.split('/')[2];
+                const key = req.url.split('/')[2]; //user/ 이후 내용, URL에 출력된 키 값은 restFront.js에서 Object.keys()로 생성
                 let body = '';
                 req.on('data',(data)=>{
                     body+=data;
                 });
                 return req.on('end',()=>{
                     console.log('PUT 본문(body):',body);
-                    users[key] = JSON.parse(body).name;
-                    return res.end(JSON.stringify(users));
+                    users[key] = JSON.parse(body).name; //DB 대체 딕셔너리?에 name 수정
+                    return res.end(JSON.stringify(users)); //다시 JSON 변환 후 종료
                 });
             }
         } else if (req.method === 'DELETE') {
             if(req.url.startsWith('/user/')) {
                 const key = req.url.split('/')[2];
-                delete users[key];
+                delete users[key]; //해당 key 값의 데이터 삭제
                 return res.end(JSON.stringify(users));
             }
         }
